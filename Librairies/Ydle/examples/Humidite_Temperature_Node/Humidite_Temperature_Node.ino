eb296c56cc33aa7a3851e01943a2f419c11b5f97
Xylerk <xylerkkrelyx@gmail.com>
2014-02-16 19:19:58 +0100

    Update Humidite_Temperature_Node.ino

--- Librairies/Ydle/examples/Humidite_Temperature_Node/Humidite_Temperature_Node.ino
+++ Librairies/Ydle/examples/Humidite_Temperature_Node/Humidite_Temperature_Node.ino
@@ -1,7 +1,7 @@
 /* Author : Fabrice Scheider AKA Denia
-* Description : Node envoyant humidit� et temp�rature en utilisant le protocole radio ydle.
-* Note : J'utilise dans cette librairie un DHT11, il est donc n�c�ssaire que vous l'ayez dans 
-* votre r�pertoire librairies pour que le sketch compile.
+* Description : Node envoyant humidité et température en utilisant le protocole radio ydle.
+* Note : J'utilise dans cette librairie un DHT11, il est donc nécessaire que vous l'ayez dans 
+* votre répertoire librairies pour que le sketch compile.
 * Licence : CC-BY-SA
 */
 
@@ -9,13 +9,15 @@
 #include "Ydle.h"
 #include "dht11.h"
 
-#define RX_PIN 2
+#define RX_PIN 12
 #define TX_PIN 10
-#define BT_PIN 1
+#define BT_PIN 3
 
 #define DELAY_SEND 15000
 
 dht11 dht;
+
+#define DHT_PIN 9
 
 ydle y(RX_PIN, TX_PIN, BT_PIN);
 
@@ -39,27 +41,36 @@ void loop()
 		cur_time = millis();
 		if(cur_time - last_send >= DELAY_SEND){
 			last_send = cur_time;
-			// code de r�cup�ration des informations venant du capteurs
-			if(dht.read(5) == DHTLIB_OK){
+			// code de récupération des informations venant du capteurs
+			if(dht.read(DHT11_PIN) == DHTLIB_OK){
 				
 				
-				// Cr�ation de la frame qui va �tre envoy�e
+				// Création de la frame qui va être envoyée
 				Frame_t frame;
-				// On y ins�re les diff�rentes valeurs dont nous avons besoin
+				// On y insère les différentes valeurs dont nous avons besoin
 			
-				/** Nous demandons un accus� r�c�ption.
-				 * La biblioth�que va s'occuper seule de l'acquitement
-				 * Si aucun acquitement n'est re�u au bout d'une seconde
-				 * elle proc�dera � un nouvel envois. Si au bout de 3 envois successifs
-				 * aucun acquittement n'est re�u, nous consid�rons la trame comme perdue.
+				/** Nous demandons un accusé réception.
+				 * La bibliothèque va s'occuper seule de l'acquittement
+				 * Si aucun acquittement n'est reçu au bout d'une seconde
+				 * elle procédera à un nouvel envois. Si au bout de 3 envois successifs
+				 * aucun acquittement n'est reçu, nous considérons la trame comme perdue.
 				 */
 				y.dataToFrame(&frame, YDLE_TYPE_STATE_ACK);
 			
-				/** Nous ajoutons nos valeurs que nous avons r�cup�r�es aupr�s du capteur	
+				/** Nous ajoutons la valeur de l'humidité que nous avons récupérée auprès du capteur	
 				 */
 				y.addData(&frame, YDLE_DATA_HUMIDITY, (int)dht.humidity);
-				y.addData(&frame, YDLE_DATA_DEGREEC, (int)dht.temperature);
-				/** Nous proc�dons � l'envois de la trame.
+				/** Nous procédons à l'envois de la trame.
+				 *
+				 */ 
+				y.send(&frame);
+				
+				// Nous créons une nouvelle frame pour la température
+				y.dataToFrame(&frame, YDLE_TYPE_STATE_ACK);
+				/** Nous ajoutons la valeur de la température que nous avons récupérée auprès du capteur	
+				 */
+				y.addData(&frame, YDLE_DATA_DEGREEC, (int)(10*dht.temperature);
+				/** Nous procédons à l'envois de la trame.
 				 *
 				 */ 
 				y.send(&frame);
